a07356f6e92d750bc62d4753eb16d46122c1cc57
oussamabenalisst <benalio248@gmail.com>
2025-06-25 13:27:53 +0100

    feat: enhance addTodo command and improve command titles in package.json

--- extension.js
+++ extension.js
@@ -11,10 +11,13 @@ function activate(context) {
         prompt: "Enter your task",
       });
       if (task) {
+        const todos = context.globalState.get("donestTodos", []);
+        todos.push(`${task}`);
+        context.globalState.update("donestTodos", todos);
         const editor = vscode.window.activeTextEditor;
         if (editor) {
           editor.edit((editBuilder) => {
-            editBuilder.insert(editor.selection.active, `- [ ] ${task}\n`);
+            editBuilder.insert(editor.selection.active, `${task}\n`);
           });
         }
       }
@@ -22,6 +25,24 @@ function activate(context) {
   );
 
   context.subscriptions.push(disposable);
+
+  let showTodosDisposable = vscode.commands.registerCommand(
+    "DoNest.ShowTodos",
+    async () => {
+      const todos = context.globalState.get("donestTodos", []);
+      if (todos.length > 0) {
+        vscode.window.showQuickPick(todos, {
+          placeHolder: "Your saved TODOs:",
+        });
+      } else {
+        vscode.window.showInformationMessage(
+          "No TODOs found in settings.json."
+        );
+      }
+    }
+  );
+
+  context.subscriptions.push(showTodosDisposable);
 }
 
 // This method is called when your extension is deactivated
